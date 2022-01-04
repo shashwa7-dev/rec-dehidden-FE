54e3dc0b92b78ae3bbfbff7e62be06e22828d86e
shashwa7-dev <shashwa7.dev@gmail.com>
2022-01-04 19:06:13 +0530

    changed 404 page

--- src/components/404.js
+++ src/components/404.js
@@ -2,10 +2,12 @@ import { Link } from "react-router-dom"
 
 const NotFound = () => {
   return (
-    <div className="not-found">
-      <h2>Sorry</h2>
-      <p>That page cannot be found</p>
-      <Link to="/rcb/1">Exit!</Link>
+    <div className="not-found" style={{ height:'100vh', width:'100vw', display:'flex', flexDirection: 'column', justifyContent:'center', alignItems:'center', magin:'auto',fontSize:"3rem"}}>
+      <h2>Sorry :(</h2>
+      <p>Wrong Route!</p>
+      <Link to="/rcb/1">
+       <button style={{padding:"1rem", outline: 'none', borderRadius: '5px', fontSize:'1rem', fontWeight:'bold',  backgroundColor:'wheat'}}>Redirect to RCB home!</button> 
+      </Link>
     </div>
   );
 }
